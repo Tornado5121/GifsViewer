cdec4e3cb5a14b988e78fab8ddb3bdafd268174b
Roman Zhadko <zhar.andr.developer.wwl@gmail.com>
2024-11-28 05:10:17 +0200

    clear code

--- app/src/main/java/com/zhadko/gifyviewer/extensions/GlideImageLoader.kt
+++ app/src/main/java/com/zhadko/gifyviewer/extensions/GlideImageLoader.kt
@@ -1,8 +1,10 @@
 package com.zhadko.gifyviewer.extensions
 
+import android.annotation.SuppressLint
 import android.widget.ImageView
 import com.bumptech.glide.RequestManager
 
+@SuppressLint("CheckResult")
 fun RequestManager.loadAsGif(link: String?, imageView: ImageView, placeholder: Int?) {
     this.asGif().load(link).apply {
         if (placeholder != null) {
