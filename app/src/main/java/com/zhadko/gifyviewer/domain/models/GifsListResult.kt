a3552d667c6d9b7f7872b5e5350968c177cd28a1
Roman Zhadko <zhadkoroman@gmail.com>
2024-12-03 04:15:34 +0200

    clear code

--- app/src/main/java/com/zhadko/gifyviewer/domain/models/GifsListResult.kt
+++
@@ -1,9 +0,0 @@
-package com.zhadko.gifyviewer.domain.models
-
-import com.zhadko.gifyviewer.data.network.models.Data
-
-sealed class GifsListResult {
-    data object EmptyResult : GifsListResult()
-    data class NotEmptyResult(val gifsList: List<Data>) : GifsListResult()
-    data class Error(val pair: Pair<String, String>) : GifsListResult()
-}
