7467825c3110409d7ab565f886ea54fc745cbbae
Roman Zhadko <zhadkoroman@gmail.com>
2024-12-03 04:16:06 +0200

    clear code

--- app/src/main/java/com/zhadko/gifyviewer/data/network/models/NetworkResult.kt
+++
@@ -1,6 +0,0 @@
-package com.zhadko.gifyviewer.data.network.models
-
-sealed class NetworkResult {
-    data class Success(val data: GifResponse) : NetworkResult()
-    data class Error(val pair: Pair<String, String>) : NetworkResult()
-}
